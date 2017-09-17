2a15e0cc52e8ee173016fa3fd56f304e915d2936
shirmen <migaakuz@gmail.com>
2017-09-17 21:29:32 +0900

    refactor node class

--- src/com/miga/datastructures/Node.java
+++ src/com/miga/datastructures/Node.java
@@ -1,9 +1,10 @@
 package com.miga.datastructures;
 
 class Node {
-    Object data;
-    Node next;
-    Node prev;
+
+    private Object data;
+    private Node next;
+    private Node prev;
 
     public Node(Object data) {
         this.data = data;
@@ -13,34 +14,35 @@ class Node {
         this.data = data;
         this.next = next;
     }
-    
+
     public Node(Object data, Node prev, Node next) {
         this.data = data;
         this.prev = prev;
         this.next = next;
     }
-    
+
     public void setData(Object data) {
         this.data = data;
     }
-    
+
     public Object getData() {
         return data;
     }
-    
+
     public void setPrev(Node prev) {
         this.prev = prev;
     }
-    
+
     public Node getPrev() {
         return prev;
     }
-    
+
     public void setNext(Node next) {
         this.next = next;
     }
-    
+
     public Node getNext() {
         return next;
     }
+
 }
